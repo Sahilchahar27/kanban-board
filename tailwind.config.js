03d64f47b6716b6c367405b0e60b8408726c39b3
Sahilchahar27 <sahilchahar28102003@gmail.com>
2024-09-09 18:28:23 +0530

    Added Tailwind CSS configuration file

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 module.exports = {
     content: [
-      './src/**/*.{js,jsx,ts,tsx}', // Adjust this path according to your project structure
+      './src/**/*.{js,jsx,ts,tsx}', 
     ],
     theme: {
       extend: {},
